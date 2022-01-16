8828415893b77513704aa8d5c8dd14e7a409f659
gpalmeida <92654431+gpalmeida@users.noreply.github.com>
2022-01-16 20:32:18 -0300

    Add files via upload

---
+++ logado.js
@@ -0,0 +1,5 @@
+function consulta(){alert("Você não tem consultas agendadas!")}
+function mensagemInd(){alert("Em breve você poderá acessar.")}
+function logout(){alert("Você está deslogando, obrigado por usar a PSICO.")}
+function suporte(){alert("Entre em contato com o suporte@psico.com ou 0800 123 456, todos os dias das 8:00 ás 16:00.")}
+// 
